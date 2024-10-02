138a80528725bb39d3045089b2a27aa9a551064c
Arredondo-Juan <juan_arredondo@icloud.com>
2024-10-01 19:39:15 -0500

    Initial Commit

---
+++ Split/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Split
+//
+//  Created by Juan Arredondo on 10/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Split/SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitApp.swift
+//  Split
+//
+//  Created by Juan Arredondo on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
